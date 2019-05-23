8bf01d96ebfec136a153ccdcac2c1c9be533f93a
Eric <eric.jiang@monash.edu>
2019-05-23 21:23:22 +1000

    improved logic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import React from "react";
+import React from 'react';
 
-import Header from "./components/Header";
-import Hero from "./components/Hero/Hero";
-
+import Header from './components/Header';
+import Hero from './components/Hero/Hero';
+import moment from 'moment-timezone';
 function App() {
-  return (
-    <div className="app">
-      <Header />
-      <div
-        style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-          display: "flex"
-        }}
-      >
-        <Hero />
-      </div>
-    </div>
-  );
+	return (
+		<div style={{ display: 'flex', flexDirection: 'column' }}>
+			<Header />
+			<div
+				style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}
+			>
+				<div
+					style={{
+						height: '50vh',
+						flex: 0.5,
+						justifyContent: 'center',
+						alignItems: 'center',
+						display: 'flex'
+					}}
+				>
+					<Hero />
+				</div>
+				<div
+					style={{
+						backgroundColor: 'var(--primary-color)',
+						color: 'white',
+						flex: 1
+					}}
+				>
+					Schedule
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,33 +1,37 @@
-import React from "react";
-import DarkLogo from "../../img/logo-dark.png";
-import WiredLogo from "../../img/wired-logo.png";
+import React from 'react';
+import DarkLogo from '../../img/logo-dark.png';
+import WiredLogo from '../../img/wired-logo.png';
 
 function Hero() {
-  return (
-    <div style={{ display: "flex" }}>
-      <div style={{ marginRight: 30, flex: 1 }}>
-        <img src={DarkLogo} style={{ height: "100%" }} />
-      </div>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          color: "var(--primary-color)"
-        }}
-      >
-        <div>
-          <img src={WiredLogo} /> presents...
-        </div>
-        <div>
-          <span style={{ fontFamily: "Courier", fontSize: 50 }}>
-            Bit by Bit Hack
-          </span>
-        </div>
-        <div>2019</div>
-      </div>
-    </div>
-  );
+	return (
+		<div
+			style={{
+				display: 'flex',
+				flexWrap: 'wrap',
+				paddingLeft: 5,
+				paddingRight: 5
+			}}
+		>
+			<div
+				style={{
+					display: 'flex',
+					flexDirection: 'column',
+					justifyContent: 'center',
+					color: 'var(--primary-color)'
+				}}
+			>
+				<div>
+					<img src={WiredLogo} style={{ height: 20 }} /> presents...
+				</div>
+				<div>
+					<h1 style={{ fontFamily: 'Courier', fontSize: 70, fontWeight: 500 }}>
+						Bit by Bit Hack
+					</h1>
+				</div>
+				<div style={{ fontSize: 70 }}>2019</div>
+			</div>
+		</div>
+	);
 }
 
 export default Hero;
